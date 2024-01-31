346b12340fe590a338528712026d519445be8624
Nathanael Blocher <blocher.nathanael@gmail.com>
2024-01-31 06:50:26 -0800

    Added GET ALL and GET ONE routes and listener

--- server.js
+++ server.js
@@ -22,3 +22,43 @@ const app = express();
 // middlewares
 app.use(express.json(), express.static('client'));
 
+// get all route
+app.get("/people", (req, res) => {
+    pool.query('select * from people')
+        .then((results) => {
+            try {
+                res.send(results.rows); return;
+            }
+            catch (err) {
+                console.error(err.message);
+                res.sendStatus(500);
+            }
+        })
+});
+
+// get one route
+app.get("/people/:id", (req, res) => {
+    const { id } = req.params;
+    if (isNaN(+id)) {
+        res.sendStatus(400); return;
+    }
+    pool.query("select * from people where id = $1", [id])
+        .then((results) => {
+            try {
+                if (results.rowCount < 1) {
+                    res.sendStatus(404); return;
+                } else {
+                    res.send(results.rows[0]); return;
+                }
+            }
+            catch (err) {
+                console.error(err.message);
+                res.sendStatus(500);
+            }
+        })
+});
+
+// tell app to listen on port
+app.listen(port, () => {
+    console.log('Server listening on port ' + port);
+})
