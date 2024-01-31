46dbe04c2e7b0d8aa282c3fc03ae12673df2cdf1
Nathanael Blocher <blocher.nathanael@gmail.com>
2024-01-31 09:06:36 -0800

    script.js now adds basic text to the DOM

--- client/script.js
+++ client/script.js
@@ -1,1 +1,22 @@
 console.log('Script is working!');
+
+let currentData = {};
+
+// fetch call to get all our data
+fetch("/people")
+.then((data) => data.json())
+.then((results) => currentData = results);
+
+const body = document.body;
+const resultsButton = document.querySelector("#results-button");
+
+resultsButton.addEventListener('click', (e) => {
+    for (let index in currentData) {
+        // create a div
+        const div = document.createElement('div');
+        // add text to that div
+        div.textContent = currentData[index].name;
+        // append that div to the body
+        body.appendChild(div);
+    }
+});
