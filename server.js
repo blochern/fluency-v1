765776708f52aa19a385a377fa8f6a381b28ce26
Nathanael Blocher <blocher.nathanael@gmail.com>
2024-01-31 08:59:42 -0800

    Added and tested post, put, and delete routes

--- server.js
+++ server.js
@@ -58,6 +58,74 @@ app.get("/people/:id", (req, res) => {
         })
 });
 
+// post one route
+app.post("/people", (req, res) => {
+    const { name, age, paid_rent } = req.body;
+    // validate incoming data types
+    if (!name || isNaN(+age) || typeof paid_rent !== 'boolean') {
+        res.sendStatus(400); return;
+    }
+    pool.query('insert into people (name, age, paid_rent) values ($1, $2, $3) returning *',
+        [name, age, paid_rent])
+        .then((results) => {
+            try {
+                res.status(201).send(results.rows[0]); return;
+            }
+            catch (err) {
+                console.error(err.message);
+                res.sendStatus(500);
+            }
+        });
+});
+
+// put one route
+app.put("/people/:id", (req, res) => {
+    // destruct id
+    const { id } = req.params;
+
+    // destruct request object
+    const { name, age, paid_rent } = req.body;
+
+    // validate data
+    if (!name || isNaN(+age) || typeof paid_rent !== 'boolean') {
+        res.sendStatus(400); return;
+    }
+
+    pool.query("update people set name = $1, age = $2, paid_rent = $3 where id = $4 returning *",
+    [name, age, paid_rent, id])
+    .then((results) => {
+        try {
+            if (results.rowCount < 1) {
+                res.sendStatus(404); return;
+            }
+            res.send(results.rows[0]); return;
+        }
+        catch (err) {
+            console.error(err.message);
+            res.sendStatus(500);
+        }
+    })
+});
+
+// delete one route
+app.delete("/people/:id", (req, res) => {
+    // destruct id
+    const { id } = req.params;
+    pool.query('delete from people where id = $1 returning *', [id])
+    .then((results) => {
+        try {
+            if (results.rowCount < 1) {
+                res.sendStatus(404); return;
+            }
+            res.send(results.rows[0]); return;
+        }
+        catch (err) {
+            console.error(err.message);
+            res.sendStatus(500);
+        }
+    });
+});
+
 // tell app to listen on port
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
